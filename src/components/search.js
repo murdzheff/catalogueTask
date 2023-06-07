75e7284e0304877c70286c620ed0d49a3cc785bc
murdzheff <murdzheff@tuta.io>
2023-06-07 09:55:36 +0300

    debounce search added

--- src/components/search.js
+++ src/components/search.js
@@ -1,29 +1,42 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import {
-  searchProductsByTitle,
-} from '../slices/productsSlice';
+import { searchProductsByTitle } from '../slices/productsSlice';
 import { setSearchResultsToProducts } from '../slices/productsSlice';
-
 
 const ProductSearch = () => {
   const [keyword, setKeyword] = useState('');
   const dispatch = useDispatch();
 
-  //search functionality together 
-  //with equalizing the products shown with the search results
+  const debounce = (func, delay) => {
+    let timeoutId;
+
+    return function (...args) {
+      clearTimeout(timeoutId);
+
+      timeoutId = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
+  };
+
   const handleSearch = async () => {
     await dispatch(searchProductsByTitle(keyword));
     dispatch(setSearchResultsToProducts());
   };
 
-
-
+  const debouncedSearch = debounce(handleSearch, 500);
 
   return (
     <div className='searchContainer'>
-      <input placeholder='Search for items' type="text" value={keyword} onChange={(e) => setKeyword(e.target.value)} />
-      <button onClick={handleSearch}>Search</button>
+      <input
+        placeholder='Search for items'
+        type="text"
+        value={keyword}
+        onChange={(e) => {
+          setKeyword(e.target.value);
+          debouncedSearch();
+        }}
+      />
     </div>
   );
 };
